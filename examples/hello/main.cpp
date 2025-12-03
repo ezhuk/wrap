b2c3da722af523f05c54cb8ad7ee87ec01938caf
Eugene Zhuk <2105905+ezhuk@users.noreply.github.com>
2025-12-02 17:27:47 -0800

    Add response stub

--- examples/hello/main.cpp
+++ examples/hello/main.cpp
@@ -5,9 +5,9 @@
 int main(int argc, char** argv) {
   wrap::App app;
 
-  app.get("/hello", [](wrap::Request const& req, proxygen::ResponseBuilder& res) {
+  app.get("/hello", [](wrap::Request const& req, wrap::Response& res) {
     fmt::print("{} {}\n", req.getMethod(), req.getURL());
-    res.status(200, "OK").body("{}").sendWithEOM();
+    res.status(200, "OK").body("{}");
   });
 
   app.run("0.0.0.0", 8080);
