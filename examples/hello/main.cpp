f7b43504edd00734f008efd1c31a24bdb0d59d32
Eugene Zhuk <2105905+ezhuk@users.noreply.github.com>
2025-12-03 21:03:06 -0800

    Add param stub

--- examples/hello/main.cpp
+++ examples/hello/main.cpp
@@ -2,12 +2,14 @@
 
 #include "wrap/app.h"
 
-int main(int argc, char** argv) {
-  wrap::App app;
+using namespace wrap;
 
-  app.get("/hello", [](wrap::Request const& req, wrap::Response& res) {
-    fmt::print("{} {}\n", req.getMethod(), req.getURL());
-    res.status(200, "OK").body("{}");
+int main(int argc, char** argv) {
+  App app;
+
+  app.get("/users/:id", [](Request const& req, Response& res) {
+    fmt::print("id={}\n", req.getParam("id"));
+    res.status(200, "OK").body(R"({})");
   });
 
   app.run("0.0.0.0", 8080);
