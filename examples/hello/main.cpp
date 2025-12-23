2b369557315b6475df10fd6976ad623a2895699f
Eugene Zhuk <2105905+ezhuk@users.noreply.github.com>
2025-12-22 18:48:26 -0800

    Add typed get

--- examples/hello/main.cpp
+++ examples/hello/main.cpp
@@ -25,6 +25,21 @@ int main(int argc, char** argv) {
     fmt::print("id={}\n", id);
   });
 
+  app.get<User>("/users/{id}", [](std::string const& id) {
+    fmt::print("id={}\n", id);
+    User user;
+    user.email = "user@example.com";
+    return user;
+  });
+
+  app.get<std::vector<User>>("/users", []() {
+    User user1;
+    user1.email = "user1@example.com";
+    User user2;
+    user2.email = "user2@example.com";
+    return std::vector<User>{user1, user2};
+  });
+
   app.run("0.0.0.0", 8080);
   return 0;
 }
