dcb4604e7f25af4bce5ab88694a5a7c3517f3670
Eugene Zhuk <2105905+ezhuk@users.noreply.github.com>
2025-12-28 23:42:45 +0000

    Revise the sample code

--- examples/hello/main.cpp
+++ examples/hello/main.cpp
@@ -7,7 +7,7 @@ using namespace wrap;
 int main(int argc, char** argv) {
   App app;
 
-  app.get("/", []() { return "Hello world!"; });
+  app.get("/", []() { return R"({"message":"Hello, world!"})"; });
 
   app.run("0.0.0.0", 8080);
   return 0;
