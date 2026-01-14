b04404cf5539f6b6552311a32f7208802faef4d4
Eugene Zhuk <2105905+ezhuk@users.noreply.github.com>
2026-01-13 17:01:59 -0800

    Add router stub

---
+++ examples/router/main.cpp
@@ -0,0 +1,16 @@
+#include <fmt/base.h>
+
+#include "wrap/app.h"
+
+using namespace wrap;
+
+int main(int argc, char** argv) {
+  App app;
+
+  Router users(app, "/users");
+  users.get("/", []() { return R"({"users":[]})"; });
+  users.get("/{id:int}", [](int id) { return fmt::format(R"({{"id":{}}})", id); });
+
+  app.run("0.0.0.0", 8080);
+  return 0;
+}
