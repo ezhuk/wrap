65a6fd279a74761a300e0f55ec2022701b5e07cb
Eugene Zhuk <2105905+ezhuk@users.noreply.github.com>
2026-01-15 22:24:53 -0800

    Add basic static files support

---
+++ examples/static/main.cpp
@@ -0,0 +1,16 @@
+#include <fmt/base.h>
+
+#include "wrap/app.h"
+
+using namespace wrap;
+
+int main(int argc, char** argv) {
+  App app;
+
+  app.use(middleware::logger());
+
+  app.get("/", serve_static("./public"));
+
+  app.run("0.0.0.0", 8080);
+  return 0;
+}
