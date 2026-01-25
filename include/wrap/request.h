3e841ccaceff5f803fc0bd1cceca292f1d152dfa
Eugene Zhuk <2105905+ezhuk@users.noreply.github.com>
2026-01-25 14:47:22 -0800

    Refactor handlers

---
+++ include/wrap/request.h
@@ -0,0 +1,41 @@
+#pragma once
+
+#include <folly/json/json.h>
+#include <proxygen/lib/http/HTTPMessage.h>
+
+#include <unordered_map>
+
+namespace wrap {
+class Request final {
+public:
+  Request(proxygen::HTTPMessage const* msg, folly::IOBuf* body) : msg_(msg), body_(body) {}
+  ~Request() = default;
+
+  std::string getMethod() const { return msg_->getMethodString(); }
+
+  std::string getURL() const { return msg_->getURL(); }
+
+  std::string getParam(std::string const& name) const {
+    auto iter = params_.find(name);
+    if (params_.end() != iter) {
+      return iter->second;
+    }
+    return "";
+  }
+
+  void setParam(std::string const& name, std::string const& data) { params_[name] = data; }
+
+  std::string getQueryParam(std::string const& name) const {
+    return msg_->getDecodedQueryParam(name);
+  }
+
+  std::string body() const { return body_ ? body_->toString() : std::string{}; }
+
+  folly::dynamic json() const { return folly::parseJson(body()); }
+
+private:
+  proxygen::HTTPMessage const* msg_;
+  folly::IOBuf* body_;
+  std::unordered_map<std::string, std::string> params_;
+};
+}  // namespace wrap
--- include/wrap/static.h
+++ include/wrap/static.h
@@ -1,5 +1,8 @@
 #pragma once
 
+#include <filesystem>
+#include <fstream>
+
 #include "app.h"
 
 namespace wrap {
