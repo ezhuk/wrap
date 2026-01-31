8159151b9613d12b624074e7c3017c63a7a1cc32
Eugene Zhuk <2105905+ezhuk@users.noreply.github.com>
2026-01-31 23:02:49 +0000

    Add gflags in the hello example

--- examples/hello/main.cpp
+++ examples/hello/main.cpp
@@ -1,14 +1,20 @@
 #include <fmt/base.h>
+#include <gflags/gflags.h>
 
 #include "wrap/app.h"
 
 using namespace wrap;
 
+DEFINE_string(host, "0.0.0.0", "Host to listen on");
+DEFINE_int32(port, 8080, "Port to listen on");
+
 int main(int argc, char** argv) {
+  gflags::ParseCommandLineFlags(&argc, &argv, true);
+
   App app;
 
   app.get("/", []() { return R"({"message":"Hello, world!"})"; });
 
-  app.run("0.0.0.0", 8080);
+  app.run(FLAGS_host, FLAGS_port);
   return 0;
 }
