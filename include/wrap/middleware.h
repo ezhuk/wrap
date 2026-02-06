c72681fd2e1a0dea762102faffa2a5426be6bb02
Eugene Zhuk <2105905+ezhuk@users.noreply.github.com>
2026-02-06 02:11:12 +0000

    Add tracer middleware

--- include/wrap/middleware.h
+++ include/wrap/middleware.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <atomic>
+
 #include "wrap/handler.h"
 
 namespace wrap {
@@ -11,6 +13,18 @@ inline Middleware logger() {
     return [next = std::move(next)](Request const& req, Response& res) {
       next(req, res);
       fmt::print("{} {}\n", req.getMethod(), req.getURL());
+    };
+  };
+}
+
+inline Middleware tracer(std::string prefix = {}) {
+  static std::atomic<uint64_t> counter{1};
+  return [prefix = std::move(prefix)](Handler next) {
+    return [next = std::move(next), prefix](Request const& req, Response& res) {
+      res.header(
+          "X-Request-Id", prefix + std::to_string(counter.fetch_add(1, std::memory_order_relaxed))
+      );
+      next(req, res);
     };
   };
 }
